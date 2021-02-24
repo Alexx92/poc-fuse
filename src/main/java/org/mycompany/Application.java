b885f8a59a9a24d62dd16b5b9956b2f841486eb6
hcastror.srv <hcastror.srv@ips.gob.cl>
2021-02-23 22:54:16 -0300

    Segunda version

--- src/main/java/org/mycompany/Application.java
+++ src/main/java/org/mycompany/Application.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2005-2016 Red Hat, Inc.
+ *  Copyright 2005-2018 Red Hat, Inc.
  *
  *  Red Hat licenses this file to you under the Apache License, version
  *  2.0 (the "License"); you may not use this file except in compliance
@@ -13,10 +13,13 @@
  *  implied.  See the License for the specific language governing
  *  permissions and limitations under the License.
  */
-package cl.poc.bean;
+package org.mycompany;
 
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 /**
@@ -27,16 +30,15 @@ import org.springframework.context.annotation.ImportResource;
 public class Application {
 
     // must have a main method spring-boot can run
-//  @Override
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    @Bean
+    ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean servlet = new ServletRegistrationBean(
+            new CamelHttpTransportServlet(), "/rest/*");
+        servlet.setName("CamelServlet");
+        return servlet;
+    }
 
-//    @Override
-//    public void configure() throws Exception {
-//        from("timer://foo?period=5000")
-//            .setBody().constant("Hello World")
-//            .log(">>> ${body}");
-//    }
 }
